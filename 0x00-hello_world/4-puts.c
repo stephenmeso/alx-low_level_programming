e5752b981ae9ce1fa35baa126a262b78dc174e11
stephenmeso <okolostephenie09@gmail.com>
2022-09-12 06:21:13 -0700

    WGCDDNE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
  * Description: using the main function
  *
- * Return always 0 (Success)
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
