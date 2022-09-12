241ae16df5df0e659c70f119531e38eab8d61fdf
stephenmeso <okolostephenie09@gmail.com>
2022-09-12 07:37:42 -0700

    gg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - prints exactly "and that piece of art is useful"
- * Return: Always 0 (Successs)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
-	return (1);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+return (1);
 }
