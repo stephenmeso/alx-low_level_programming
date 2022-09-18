ab2369cac02fb7e1f5c76528211a7789fcd1eadb
stephenmeso <okolostephenie09@gmail.com>
2022-09-18 12:03:26 -0700

    changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,12 @@
  * _islower - Checks if a character is lowercase
  * @c: The character to be checked
  *
- * Return: 1 ifcharacter is lowercase, 0 otherwise
+ * Return: 1 if character is lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z' )
+	if (c >= 'a' && c <= 'z' )
 		return (1);
 	else
 		return (0);
